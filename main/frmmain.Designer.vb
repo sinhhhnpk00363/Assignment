00511e7e6d16f75cbf642e77f34026f23f5d069b
https://github.com/sinhhhnpk00363/Assignment.git <https://github.com/sinhhhnpk00363/Assignment.git>
2015-03-01 12:33:06 +0700

    a

---
+++ main/frmmain.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SẢNPHẨMToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SẢNPHẨMToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(583, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SẢNPHẨMToolStripMenuItem
+        '
+        Me.SẢNPHẨMToolStripMenuItem.Name = "SẢNPHẨMToolStripMenuItem"
+        Me.SẢNPHẨMToolStripMenuItem.Size = New System.Drawing.Size(80, 20)
+        Me.SẢNPHẨMToolStripMenuItem.Text = "SẢN PHẨM"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(583, 262)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "QUẢN LÝ BÁN HÀNG"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents SẢNPHẨMToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ main/frmmain.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmmain
+
+    Private Sub SẢNPHẨMToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SẢNPHẨMToolStripMenuItem.Click
+        Dim sanpham = New frmmain()
+
+
+    End Sub
+End Class
